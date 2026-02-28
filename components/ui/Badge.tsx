2fc5b895d6aa2112db724c6346299f1aaf3e90ee
arty69x <arty69xx@gmail.com>
2026-03-01 02:38:13 +0700

    Delete components directory

--- components/ui/Badge.tsx
+++
@@ -1,3 +0,0 @@
-export default function Badge({ children }: { children: React.ReactNode }) {
-  return <span className="border border-gray-200 px-2 py-1 text-[10px] font-black uppercase tracking-[0.3em]">{children}</span>;
-}
--- components/ui/Skeleton.tsx
+++
@@ -1,3 +0,0 @@
-export default function Skeleton({ className = "" }: { className?: string }) {
-  return <div className={`animate-pulse bg-gray-100 ${className}`} />;
-}
